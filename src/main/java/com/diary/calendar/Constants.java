74be9dc98e00615f6f582bad9006b6c84805e880
Aliaksandr Padalka <aliaksandr_padalka@epam.com>
2015-07-07 13:14:58 +0300

    fix checkstyle warnings step 2

--- src/main/java/com/diary/calendar/Constants.java
+++ src/main/java/com/diary/calendar/Constants.java
@@ -1,6 +1,7 @@
 package com.diary.calendar;
 
 /**
+ * {@inheritDoc}
  * <h3>Class of constants</h3>
  *
  * @author Aliaksandr_Padalka
@@ -8,34 +9,41 @@ package com.diary.calendar;
 public final class Constants {
 
     /**
+     * {@inheritDoc}
      * <h3>Constants of user access</h3>
      */
     public static final class Access {
 
         /**
+         * {@inheritDoc}
          * <h3>Constant ANONIMOUS</h3>
          */
         public static final String ANONIMOUS = "ANONIMOUS";
     }
 
     /**
+     * {@inheritDoc}
      * <h3>Constants of page names</h3>
      */
     public static final class Pages {
 
         /**
+         * {@inheritDoc}
          * <h3>Constant URL_PAGE_HELLO</h3>
          */
         public static final String URL_PAGE_HELLO = "hello";
         /**
+         * {@inheritDoc}
          * <h3>Constant URL_PAGE_403</h3>
          */
         public static final String URL_PAGE_403 = "403";
         /**
+         * {@inheritDoc}
          * <h3>Constant URL_PAGE_ADMIN</h3>
          */
         public static final String URL_PAGE_ADMIN = "admin";
         /**
+         * {@inheritDoc}
          * <h3>Constant URL_PAGE_ERROR</h3>
          */
         public static final String URL_PAGE_ERROR = "error";
